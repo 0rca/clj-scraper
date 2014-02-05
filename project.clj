a090bf4e4104a9790bb9d5affd14cbc5c65f165b
Alex Vzorov <alex.vzorov@gmail.com>
2014-02-06 06:16:10 +0700

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scraper "0.2.2-SNAPSHOT"
+(defproject scraper "0.3.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
