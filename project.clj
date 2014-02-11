5d0fbfaacb31dc8fc6d3c78879c69c1b3051015a
Alex Vzorov <alex.vzorov@gmail.com>
2014-02-11 22:18:28 +0700

    Use real version of core.async instead of `LATEST`

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "LATEST"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [http-kit "2.1.16"]
                  [enlive "1.1.5"]
                  [clj-time "0.6.0"]
